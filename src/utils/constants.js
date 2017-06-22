15e54f1dd62a7e546b1dcac64028b088734154c3
ROBERT ALAN KISTNER <rak5124@psu.edu>
2017-06-22 15:36:49 -0400

    fix on const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,9 @@ export let navigate = {
 
 
 export let views = {
-  MONTH: 'Month',
-  WEEK: 'Week',
-  WORK_WEEK: 'Work_Week',
-  DAY: 'Day',
-  AGENDA: 'Agenda'
+  MONTH: 'month',
+  WEEK: 'week',
+  WORK_WEEK: 'work_week',
+  DAY: 'day',
+  AGENDA: 'agenda'
 }
